62865ce9c0361440bfda045850f96acfca7f0fec
RamAvtarDubey <ramavtardubey952@gmail.com>
2025-06-18 13:33:17 +0530

    Update settings.py

--- trainbooking/trainbooking/settings.py
+++ trainbooking/trainbooking/settings.py
@@ -127,7 +127,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'backend/static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
